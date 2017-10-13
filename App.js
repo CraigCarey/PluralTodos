0993312d73b20de88c3b1fd6e2012af82702e6c5
craig <sgccarey@gmail.com>
2017-10-13 15:32:19 +0100

    Added ListView to TaskList

--- App.js
+++ App.js
@@ -3,20 +3,23 @@ import TaskList from './TaskList'
 
 export default class PluralTodo extends Component {
 
-    constructor(props) {
-        super(props);
+    constructor(props, context) {
+        super(props, context);
         this.state = {
             todos: [
                 {
                     task: 'Learn React Native'
+                },
+                {
+                    task: 'Learn Redux'
                 },
             ],
         };
     }
 
     render() {
-        return (
-            <TaskList/>
-        );
+        return <TaskList
+            todos={this.state.todos}
+        />;
     }
 }
--- TaskList.js
+++ TaskList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ListView } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
@@ -9,11 +9,37 @@ const styles = StyleSheet.create({
 });
 
 export default class TaskList extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2,
+        });
+
+        this.state = {
+            dataSource: ds.cloneWithRows(props.todos),
+        }
+    }
+
+    renderRow(todo) {
+        return (
+            <Text>{todo.task}</Text>
+        );
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>Hi, this is the fagging TaskList!</Text>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={this.renderRow.bind(this)}
+                />
             </View>
         );
     }
 }
+
+TaskList.propTypes = {
+    todos: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
+};
