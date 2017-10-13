244c165eed19d32e9b0c0f23cb37af5058168561
craig <sgccarey@gmail.com>
2017-10-13 16:41:09 +0100

    Added Button

--- TaskList.js
+++ TaskList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { View, StyleSheet, ListView } from 'react-native';
+import { View, StyleSheet, ListView, TouchableHighlight, Text } from 'react-native';
 
 import TaskRow from './TaskRow'
 
@@ -11,6 +11,20 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'flex-start'
     },
+    button: {
+        height: 60,
+        borderColor: '#05A5D1',
+        borderWidth: 2,
+        backgroundColor: '#333',
+        margin: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: '#FAFAFA',
+        fontSize: 20,
+        fontWeight: '600'
+    }
 });
 
 export default class TaskList extends Component {
@@ -40,11 +54,21 @@ export default class TaskList extends Component {
                     dataSource={this.state.dataSource}
                     renderRow={this.renderRow.bind(this)}
                 />
+
+                <TouchableHighlight
+                    onPress={this.props.onAddStarted}
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText}>
+                        Add One
+                    </Text>
+                </TouchableHighlight>
             </View>
         );
     }
 }
 
 TaskList.propTypes = {
+    onAddStarted: React.PropTypes.func.isRequired,
     todos: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
 };
