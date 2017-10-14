bb576dedbfc49fe35ec084186be7a42ebdec1467
craig <sgccarey@gmail.com>
2017-10-14 22:21:17 +0100

    Filled in TaskForm

--- TaskForm.js
+++ TaskForm.js
@@ -1,5 +1,41 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TextInput, View, TouchableHighlight, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        paddingTop: 150,
+        backgroundColor: '#F7F7F7',
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#D7D7D7',
+        height: 50,
+        marginLeft: 10,
+        marginRight: 10,
+        padding: 15,
+        borderRadius: 3,
+    },
+    buttonText: {
+        fontSize: 18,
+        fontWeight: '600',
+        color: '#FAFAFA',
+    },
+    button: {
+        height: 45,
+        alignSelf: 'stretch',
+        backgroundColor: '#05A5D1',
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    cancelButton: {
+        backgroundColor: '#666',
+    },
+});
 
 export default class TaskForm extends Component {
 
@@ -11,12 +47,29 @@ export default class TaskForm extends Component {
         super(props, context);
 
         this.state = {
+            task: ''
         }
     }
 
     render() {
         return (
-            <Text>Add form comes here!</Text>
+            <View style={styles.container}>
+
+                <TextInput style={styles.input} />
+
+                <TouchableHighlight style={styles.button}>
+                    <Text style={styles.buttonText}>
+                        Add
+                    </Text>
+                </TouchableHighlight>
+
+                <TouchableHighlight style={[styles.button, styles.cancelButton]}>
+                    <Text style={styles.buttonText}>
+                        Cancel
+                    </Text>
+                </TouchableHighlight>
+
+            </View>
         );
     }
 }
